88fbb2625193d51503e98907aa696330523dd56b
Anastasiia <nastena31011993@gmail.com>
2019-05-15 19:33:02 +0300

    Add Composite project

---
+++ Composite/Group.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Composite
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public List<Person> Members { get; set; }
+
+        public Group()
+        {
+            Members = new List<Person>();
+        }
+    }
+}
---
+++ Composite/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Composite
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Gold { get; set; }
+
+        public void Stats()
+        {
+            Console.WriteLine($"{Name} has {Gold} gold");
+        }
+    }
+}
